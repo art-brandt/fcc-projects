da74c177eefbe7bd1a27ef58c2aa3d2b035249b4
Mikhail Panteleev <mihail.panteleyev@gmail.com>
2018-12-15 17:08:30 +0500

    start build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,11 @@
-const gulp = require("gulp");
-const {sh} = require('sh-thunk');
-const fs = require('fs');
-const path = require('path');
+const gulp = require('gulp');
 
-const projectsFixture = fs.readFileSync(path.join(__dirname, "projectsFixture.json"));
-const fixtures = JSON.parse(projectsFixture);
+function test(callback) {
+  console.log('done');
+  callback();
+}
 
-let buildingProjects = fixtures.sections.map((item) => {
-  let folder = item.folder
-  return item.projects.map(project => { 
-    project.section = folder;
-    return project 
-  });
-}).reduce((accum, val) => { 
-  return accum.concat(val) 
-}, []).filter(obj => { 
-  return obj.build === true 
-}); // { name : string, folder : string, build: boolen, section: string }
+test.displayName = 'test';
+test.description = 'test task';
 
-console.log(buildingProjects )
-
-gulp.task('webpack-build', sh(`cd ${buildingProjects[1].section}`));
-
+gulp.task('test', test);
