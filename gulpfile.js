a46aefb07bfd0a9ec964399a60c6f4a3e5b0b57d
Mikhail Panteleev <mihail.panteleyev@gmail.com>
2018-12-16 00:47:37 +0500

    add gulpfil task post-build all  and build all

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,42 @@
-const gulp = require('gulp');
+const gulp = require("gulp");
+const {sh} = require('sh-thunk');
+const fs = require('fs');
+const path = require('path');
 
-function test(callback) {
-  console.log('done');
-  callback();
-}
+const projectsFixture = fs.readFileSync(path.join(__dirname, "projectsFixture.json"));
+const fixtures = JSON.parse(projectsFixture);
 
-test.displayName = 'test';
-test.description = 'test task';
+let prjsForBuild = fixtures.sections.map((section) => {
+  let sectionFolder = section.folder
+  return section.projects.map(project => { 
+    project.path = `./${sectionFolder}/${project.folder}/`;
+    project.name = project.folder;
+    delete project.folder;
+    return project;
+  });
+}).reduce((accum, val) => {
+  return accum.concat(val) 
+}, []).filter(obj => {
+  return obj.build === true;
+}).map(item => {
+  delete item.build;
+  return item;
+}); // { name : string, path: string }
 
-gulp.task('test', test);
+prjsForBuild.forEach(project => {
+  gulp.task('build:' + project.name, sh(`cd ${project.path} && npm run build`));
+  gulp.task('heroku-postbuild:' + project.name, 
+    sh(`cd ${project.path} && npm install && npm run build`
+  ));
+});
+
+gulp.task('build:all', gulp.series(
+    prjsForBuild.map(project => { return 'build:'+ project.name })
+));
+
+gulp.task('heroku-postbuild:all', gulp.series(
+  prjsForBuild.map(project => { return 'heroku-postbuild:'+ project.name })
+));
+
+
+
